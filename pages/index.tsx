5fb0796fa1b2df797ee4d55d584eb0976e336737
杨飞 <yangfei86@vip.qq.com>
2022-08-16 11:17:02 +0800

    时间显示修复，标题图显示优化

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,23 +81,29 @@ const Home: NextPage<GetStaticProps> = ({ name }) => {
             return (
               <div
                 className="w-full flex bg-white box-border overflow-hidden rounded-md shadow-md"
-                style={{ height: 200 }}
+                style={{ height: 220 }}
                 key={row.id}
               >
-                <img
-                  className="h-full"
-                  style={{ width: 360 }}
-                  src={`http://localhost:1337${row.image.url}`}
-                  alt={row.image.name}
+                <div
+                  className="h-full bg-cover bg-center"
+                  style={{
+                    backgroundImage:
+                      row.image.provider === 'local'
+                        ? `url('http://localhost:1337${row.image.url}')`
+                        : row.image.url,
+                    width: 360,
+                  }}
                 />
                 <div className="h-full flex-1 flex flex-col items-start justify-start p-6">
-                  <div className="w-full h-8 text-2xl font-bold flex items-center">
+                  <div className="w-full h-10 text-2xl font-bold flex items-center">
                     <p>{row.title}</p>
                   </div>
-                  <div className="w-full flex-1">{row.description}</div>
+                  <div className="w-full flex-1 mt-2 text-gray-500">{row.description}</div>
                   <div className="w-full flex justify-between items-center">
-                    <div>{dayjs.utc(row.updatedAt).format('YYYY-MM-DD HH:mm:ss')}</div>
-                    <button className="btn btn-md btn-outline btn-ghost">了解更多</button>
+                    <div>{dayjs.utc(row.updatedAt).local().format('YYYY-MM-DD HH:mm:ss')}</div>
+                    <button className="btn btn-sm btn-outline btn-ghost rounded-none px-8 text-gray-400 border-gray-300">
+                      了解更多
+                    </button>
                   </div>
                 </div>
               </div>
